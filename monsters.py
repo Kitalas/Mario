13904dc1299affe684c9719d75bb13fd218686c1
MKhoroshko <margo.khoroshko@gmail.com>
2025-02-13 19:51:00 +0200

    commit forst

---
+++ monsters.py
@@ -0,0 +1,15 @@
+import os
+from pygame import *
+
+MONSTER_WIDTH, MONSTER_HEIHGT, MONSTER_COLOR = 32, 32, '#2111FF'
+ICON_DIR = os.path.dirname(__file__)
+
+class Monster:
+    def __init__(self, x, y, left, up, max_LengthUp):
+        pass
+
+    def update(self):
+        pass
+
+    def collide(self, platform):
+        pass
