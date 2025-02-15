18b3366a55d7cdfedc8b001e9de697bf9a362e31
MKhoroshko <margo.khoroshko@gmail.com>
2025-02-15 21:18:56 +0200

    commit forst

--- player.py
+++ player.py
@@ -1,2 +1,12 @@
 import os
 from pygame import *
+import block
+import monsters
+
+MOVE_SPEED = 7
+MAVE_EXTRA_SPEED = 2.5
+WIDTH, HEIGHT, COLOR = 22, 32, '#888888'
+JUMP_POWER, JUMP_EXTRA_POWER, GRAVITY = 10, 1, 0.25
+ANIMATION_DELAY, ANIMATION_SUPER_SPEED_DELAY = 0.1, 0.05
+
+ICON_DIR = os.path.dirname(__file__)
